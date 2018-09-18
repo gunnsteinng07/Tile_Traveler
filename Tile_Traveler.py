a1b531c5d4abe8ec1e0c81c77ab05878b107ab32
gunnsteinng <gunnsteinng07@ru.is>
2018-09-18 18:48:56 +0000

    vista

--- Tile_Traveler.py
+++ Tile_Traveler.py
@@ -4,8 +4,8 @@
 # Hreyfing fyrir hverja tilfærslu getur verið:
     # N => Y + 1
     # S => Y - 1
-    # A => X + 1
-    # V => X - 1
+    # E => X + 1
+    # W => X - 1
 # Fastar stærðir verði: Max = 3 og Min = 1 fyrir bæði X og Y. Notaðir eru
 # sömu Fastar fyrir X og Y þar sem í þessu tilviki þær eru 1 og 3 fyrir 
 # báða ásana (axis).
@@ -18,3 +18,23 @@
     # 2,2 er bara hægt að fara (V)estur
     # 2,3 er bara hægt að fara (A)ustur og (V)estur
     # 3,2 er bara hægt að fara (N)orður og (S)uður
+
+
+Max = 3
+Min = 1
+x = 1
+y = 1
+
+# While loopan sem ætti að nota
+while x != Max and y != Min:
+
+    if x == 1 and y == 1:
+        print("You can travel: (N)orth.")
+        movement = input("Direction: ")
+        movement = movement.lower()
+        if movement == "n":
+            x += 0
+            y += 1
+        else:
+            print("Not a valid direction!")
+        print(x,",",y)
